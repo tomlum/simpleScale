518af7f8d3c7d0768e5e7b483b9034731a745e32
tomlum <checkeredlines@gmail.com>
2018-02-25 23:38:39 -0500

    Major rewrite and example

---
+++ main.lua
@@ -0,0 +1,51 @@
+-- Example Project
+
+function love.load()
+	love.window.setTitle("SimpleScale Demo")
+	require "simpleScale"
+	love.graphics.setDefaultFilter("nearest","nearest")
+	im_ship = love.graphics.newImage("ship.png")
+	love.graphics.setBackgroundColor(100,110,140)
+
+	scaleType = 1
+	width = 300
+	height = 300
+	windowWidth = 300
+	widthHeight = 300
+	fullscreen = false
+	simpleScale.setWindow(width,height,windowWidth,widthHeight, {fullscreen = fullscreen});
+
+	function updateScale()
+		simpleScale.updateWindow(windowWidth,widthHeight, {fullscreen = fullscreen});
+	end
+end
+
+function love.update()
+	if love.keyboard.isDown("left") then
+		windowWidth = math.max(windowWidth - 50, 50)
+		fullscreen = false
+		updateScale()
+	elseif love.keyboard.isDown("right") then
+		windowWidth = windowWidth + 50
+		fullscreen = false
+		updateScale()
+	elseif love.keyboard.isDown("down") then
+		widthHeight = math.max(widthHeight - 50, 50)
+		fullscreen = false
+		updateScale()
+	elseif love.keyboard.isDown("up") then
+		widthHeight = widthHeight + 50
+		fullscreen = false
+		updateScale()
+	elseif love.keyboard.isDown("f") then
+		fullscreen = not fullscreen
+		updateScale()
+	end
+end
+
+function love.draw()
+	simpleScale.set()
+	love.graphics.print("Press\nRight: Increase Width\nLeft: Decrease Width\nUp:Increase Height\nDown: Decrease Height\nF: Toggle Fullscreen", 0, 0)
+	love.graphics.draw(im_ship,150,150,0,1,1,81/2,52/2)
+	simpleScale.unSet()
+end
--- simpleScale.lua
+++ simpleScale.lua
@@ -1,59 +1,90 @@
 simpleScale = {}
---The Game's Aspect Ratio
+--Your Game's Aspect Ratio
 local gAspectRatio
---The Screen's Aspect Ratio
-local sAspectRatio
-local xt, yt, scale = 0, 0, 1
-local gameW, gameH, screenW, screenH = 800, 600, 800, 600
-local screenType
+--The Window's Aspect Ratio
+local wAspectRatio
+--The scale between the game and the window's aspect ratio.
+simpleScale.scale = 1
 
---gameW and gameH are the width and height of the initial game
---screenW and screenH are the width and height of the final screen
-function simpleScale.setScreen(gw, gh, sw, sh, settings)	
+local xt, yt = 0, 0, 1
+local gameW, gameH, windowW, windowH = 800, 600, 800, 600
+
+-- Declares your game's width and height, and sets the window size/settings
+-- To be used instead of love.window.setMode
+--gw and gh are the width and height of the initial game
+--sw and sh are the width and height of the final window
+--settings are settings for love.window.setMode
+function simpleScale.setWindow(gw, gh, sw, sh, settings)	
 	gAspectRatio = gw/gh
-	love.window.setMode(sw,sh, settings)
-	gameW, gameH, screenW, screenH = gw, gh, love.graphics.getWidth(), love.graphics.getHeight()
-	sAspectRatio = screenW/screenH
+	gameW = gw
+	gameH = gh
+	simpleScale.updateWindow(sw, sh, settings)
+end
 
-	--Screen aspect ratio is TALLER than game
-	if gAspectRatio > sAspectRatio then
-		scale = screenW/gameW
+-- Updates the Window size/settings
+-- To be used instead of love.window.setMode
+--sw and sh are the width and height of the new Window
+--settings are settings for love.window.setMode
+function simpleScale.updateWindow(sw, sh, settings)
+	love.window.setMode(sw, sh, settings)
+	windowW, windowH = love.graphics.getWidth(), love.graphics.getHeight()
+	wAspectRatio = windowW/windowH
 
+	--Window aspect ratio is TALLER than game
+	if gAspectRatio > wAspectRatio then
+		scale = windowW/gameW
 		xt = 0
-		yt = screenH/2 - (scale*gameH)/2
+		yt = windowH/2 - (scale*gameH)/2
 
-		--Screen aspect ratio is WIDER than game
-	elseif gAspectRatio < sAspectRatio then
-		scale = screenH/gameH
-
-		xt = screenW/2 - (scale*gameW)/2
+	--Window aspect ratio is WIDER than game
+	elseif gAspectRatio < wAspectRatio then
+		scale = windowH/gameH
+		xt = windowW/2 - (scale*gameW)/2
 		yt = 0
+
+	--Window and game aspect ratios are EQUAL
 	else
-		scale = screenW/gameW
+		scale = windowW/gameW
+
 		xt = 0
 		yt = 0
 	end
-
+	simpleScale.scale = scale
 end
 
--- Transforms screen geometry. Call this at the beginning of love.draw().
-function simpleScale.transform()
+-- Transforms the game's window relative to the entire window
+-- Call this at the beginning of love.draw()
+function simpleScale.set()
 	love.graphics.translate(xt, yt)
 	love.graphics.scale(scale, scale)
 end
 
-function simpleScale.letterBox(color)
+-- Untransforms the game's window
+-- Call this at the end of love.draw
+function simpleScale.unSet(coloR, g, b, a)
+	love.graphics.scale(1/scale, 1/scale)
+	love.graphics.translate(-xt, -yt)
+
+	--Draw the Letterboxes
 	local r,g,b,a = love.graphics.getColor()
 	local originalColor = love.graphics.getColor()
-	boxColor = color or {0,0,0}
-	lg.setColor(boxColor)
-	if gAspectRatio > sAspectRatio then
-		love.graphics.rectangle("fill", 0, 0, screenW/scale, -(yt/scale)-1)
-		love.graphics.rectangle("fill", gameW, 0, screenW/scale, (yt/scale)+1)
-	elseif gAspectRatio < sAspectRatio then
-		love.graphics.rectangle("fill", 0, 0, -(xt/scale)-1, screenH)
-		love.graphics.rectangle("fill", gameW, 0, (xt/scale)+1, screenH)
-
+	local boxColor
+	if color == nil then
+		boxColor = {0,0,0}
+	elseif type(color) ~= "table" then
+		boxColor = {coloR, g, b, a}
+	else
+		boxColor = coloR
+	end
+	love.graphics.setColor(boxColor)
+	--Vertical bars
+	if gAspectRatio > wAspectRatio then
+		love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), math.abs((gameH*scale - (windowH))/2))
+		love.graphics.rectangle("fill", 0, love.graphics.getHeight(), love.graphics.getWidth(), -math.abs((gameH*scale - (windowH))/2))
+		--Horizontal bars
+	elseif gAspectRatio < wAspectRatio then
+		love.graphics.rectangle("fill", 0, 0, math.abs((gameW*scale - (windowW))/2),love.graphics.getHeight())
+		love.graphics.rectangle("fill", love.graphics.getWidth(), 0, -math.abs((gameW*scale - (windowW))/2),love.graphics.getHeight())
 	end
 	love.graphics.setColor(r,g,b,a)
 end
